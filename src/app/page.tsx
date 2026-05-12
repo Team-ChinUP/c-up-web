57dad8f61622fe7ed3fb4a712963f5ca6e8d4314
uranny <164876325+uranny@users.noreply.github.com>
2026-05-12 18:18:51 +0900

    feat : create home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,11 @@
-"use client"
+import LinkButton from "@/components/link-button";
+import MotionBox from "@/components/motion-box";
 
 export default function Home() {
   return (
-    <main>
-    </main>
+    <div className="flex flex-col gap-32 items-center">
+      <MotionBox/>
+      <LinkButton href={"/sign-in"}>대화하기</LinkButton>
+    </div>
   );
 }
