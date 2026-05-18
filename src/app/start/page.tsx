8fb043715e869096c5440461cd8d4dd57ec5e634
uranny <164876325+uranny@users.noreply.github.com>
2026-05-18 13:03:08 +0900

    feat : create main and start page

---
+++ src/app/start/page.tsx
@@ -0,0 +1,11 @@
+import LinkButton from "@/components/global/link-button";
+import MotionBox from "@/components/global/motion-box";
+
+export default function Home() {
+  return (
+    <div className="flex flex-col gap-32 items-center">
+      <MotionBox/>
+      <LinkButton href={"/main"}>대화하기</LinkButton>
+    </div>
+  );
+}
